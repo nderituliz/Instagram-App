97873c33684a7b9c7166fcd77a5b05bd58c69382
Elizabeth Nderitu <nderituliz@gmail.com>
2021-01-26 01:30:48 +0300

    added class imageForm

---
+++ gram/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Image, Profile
+
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ['image','image_name','image_caption','profile','likes','comments']
