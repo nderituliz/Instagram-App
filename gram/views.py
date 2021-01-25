2f4f8c43994d3a0cdc49f6ac1ea899f43bbdb5d1
Elizabeth Nderitu <nderituliz@gmail.com>
2021-01-26 01:18:46 +0300

    added views function photo_upload

--- gram/views.py
+++ gram/views.py
@@ -10,3 +10,18 @@ def home_page(request):
     profile = Profile.objects.all()
     
     return render(request, "index.html",{'images':images})
+
+@login_required(login_url='accounts/login')
+def photo_upload(request):
+    current_user = request.user 
+    if request.method =='POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.save()
+        return redirect('/')
+    
+    else: 
+        form = ImageForm()
+
+    return render(request,"photo_upload.html",{"form":form})
