47ff978e5e6482e7dbc0df9a869ccd894d2a5c26
Elizabeth Nderitu <nderituliz@gmail.com>
2021-01-26 01:31:40 +0300

    added class profile forms

--- gram/forms.py
+++ gram/forms.py
@@ -5,3 +5,9 @@ class ImageForm(forms.ModelForm):
     class Meta:
         model = Image
         fields = ['image','image_name','image_caption','profile','likes','comments']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        #exclude = ['username']
+        fields = ['profile', 'bio']
