d2c3dc6ac6206bc62606caf62a224eefa96f187d
Elizabeth Nderitu <nderituliz@gmail.com>
2021-01-26 01:05:03 +0300

    added views function home_page

--- gram/views.py
+++ gram/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from .models import Image, Profile
+from .forms import ImageForm, ProfileForm
 
 # Create your views here.
+def home_page(request):
+    current_user = request.user
+    images = Image.objects.all()
+    profile = Profile.objects.all()
+    
+    return render(request, "index.html",{'images':images})
