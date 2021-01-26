bbe6605f70e47d2cebf7a4ca7e74fe7976b88334
Elizabeth Nderitu <nderituliz@gmail.com>
2021-01-26 12:05:50 +0300

    registered profile,picture & comments

--- gram/admin.py
+++ gram/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import  Profile,Picture,Comments
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Picture)
+admin.site.register(Comments)
--- gram/forms.py
+++ gram/forms.py
@@ -1,13 +1,19 @@
 from django import forms
-from .models import Image, Profile
-
-class ImageForm(forms.ModelForm):
-    class Meta:
-        model = Image
-        fields = ['image','image_name','image_caption','profile','likes','comments']
+from .models import Profile,Picture,Comments
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        exclude = ['username']
-        fields = ['profile', 'bio']
+        exclude=['user']
+
+
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Picture
+        exclude =[ 'profile_details', 'like_add', 'vote_score', 'num_vote_up', 'num_vote_down','profile',]
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comments
+        exclude = ['image','user']
